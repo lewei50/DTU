6a2c0437898fd29a6770324dcaba716f9f246010
Lewei50 <service@lewei50.com>
2017-04-15 16:08:33 +0800

    Update lewei_tcp2serial.py
    remove unnecessary code , when the py was in exception and exit，it will be raise by and a Daemon(.sh)

--- lewei_tcp2serial.py
+++ lewei_tcp2serial.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf_8 -*-
 """
- Modbus乐联网透传 for python
+ Modbus tcp2serial for python
  auther gyangbo@gmail.com
  This is distributed under GNU LGPL license, see license.txt
 """
@@ -14,74 +14,69 @@ from random import *
 leweiModbusServer = "modbus.lewei50.com"
 leweiModbusPort = 9970
 
-gatewayNo = '24'#gateway No. in lewei50
-userKey = 'your_userkey_in_lewei50'
+gatewayNo = 'XX'#gateway No. in lewei50
+userKey = 'xxxxxxxxxxxx'
 
 ser=None
 #serial_port="/dev/ttyUSB0"
-serial_port=0#serial_port = 8  means serial id 9 in windows
+serial_port=5#serial_port = 8  means serial id 9 in windows
 serial_timeout=1
 serial_baud = 9600
 
 
 def run(svr_status):
-    global ser,serial_timeout,serial_port,serial_baud
-    
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_address = (leweiModbusServer, leweiModbusPort)
-    print >>sys.stderr, 'connecting to %s port %s' % server_address
-    print serial_port
     try:
+        global ser,serial_timeout,serial_port,serial_baud
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (leweiModbusServer, leweiModbusPort)
+        print >>sys.stderr, 'connecting to %s port %s' % server_address
+        ser = serial.Serial(port=serial_port, baudrate=serial_baud, bytesize=8, parity="N", stopbits=1, xonxoff=0)
         sock.connect(server_address)
-        try:
-            try:
-                if ser is None:
-                    ser=serial.Serial(port=serial_port, baudrate=serial_baud, bytesize=8, parity="N", stopbits=1, xonxoff=0)
-                    ser.setTimeout(serial_timeout)
-            except:
-                print "fail to connect serial"
-                ser.close()
-                ser=None
-            # Send data
-            message = userKey+"_"+gatewayNo
-            print >>sys.stderr, 'sending "%s"' % message
-            sock.sendall(message)
+        message = userKey + "_" + gatewayNo
+        print >> sys.stderr, 'sending "%s"' % message
+        sock.sendall(message)
+        while 1:
+            data = sock.recv(1024)
+            print >> sys.stderr, 'received "%s"' % data
+            hexShow(data)
+            '''try to send to serial'''
+            ser.write(data)
+            # wait 1 second to get feed back from serial
+            time.sleep(2)
+
+            '''read from serial'''
+            n = ser.inWaiting()
+            print(n)
+            if (n > 4):
+                serialData = ser.read(n)
+                sock.sendall(serialData)
+
+    except Exception,e:
+        print Exception,":",e
 
 
-            while 1:
-                data = sock.recv(1024)
-                print >>sys.stderr, 'received "%s"' % data
-                '''try to send to serial'''
-                try:
-                    ser.write(data)
-                    #wait 1 second to get feed back from serial
-                    time.sleep(1)
-                    
-                    '''read from serial'''
-                    n = ser.inWaiting()
-                    print(n)
-                    if(n>4):
-                        serialData = ser.read(n)
-                        sock.sendall(serialData)
-                        
-                except:
-                    print "error write to serial"
-                if data == "":
-                    sock.close()
-                    print "connection break!wait a while to reconnect"
-                    time.sleep(10 + randint(1, 60))
-                    run(0)
-                    break
 
-        finally:
-            print >>sys.stderr, 'closing socket'
-            sock.close()
-    except:
-        print "fail"
+
+
+
+
+
+
         
     
+def hexShow(argv):  
+    result = ''  
+    hLen = len(argv)  
+    for i in xrange(hLen):  
+        hvol = ord(argv[i])  
+        hhex = '%02x'%hvol  
+        result += hhex  
+    print 'hexShow:',result
+    return result
     
 
 if __name__ == "__main__":
     run(0)
     
+
